24ab9faf7eb3fac44c42a337a74dc60815da51b1
Priya <priyaagni28@gmail.com>
2016-07-11 14:17:05 +0530

    Update MainActivity.java

--- Mynavigation/app/src/main/java/com/example/priyasingh/mynavigation/MainActivity.java
+++ Mynavigation/app/src/main/java/com/example/priyasingh/mynavigation/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
